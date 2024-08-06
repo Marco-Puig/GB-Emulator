ac1d3ae7b5e436a1ad481f2d58d3c3184f843564
Marco Puig <90495366+Marco-Puig@users.noreply.github.com>
2024-08-05 23:10:25 -0400

    Update main.cpp

--- src/gbemu/main.cpp
+++ src/gbemu/main.cpp
@@ -2,9 +2,8 @@
 
 int main()
 {
-    std::ifstream file("../../roms/dmg-acid2.gb", std::ios::binary);
+    std::ifstream file("../../roms/dmg-acid2.gb", std::ios::binary); // we can use iostream for choosing the file
 
-    // file failed to open - send error
     if (!file.is_open())
     {
         std::cout << "Error opening file ../../roms/dmg-acid2.gb" << std::endl;
