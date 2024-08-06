86020517cac2fff9f2ba7fe2b0077639c976ed59
EduardoBDonovan <eddiemb26@gmail.com>
2024-08-05 21:35:56 -0700

    ED3: Build process updated

--- src/gbemu/main.cpp
+++ src/gbemu/main.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-    std::ifstream file("../../roms/dmg-acid2.gb", std::ios::binary); // we can use iostream for choosing the file
+    std::ifstream file("../work/roms/dmg-acid2.gb", std::ios::binary); // we can use iostream for choosing the file
 
     if (!file.is_open())
     {
-        std::cout << "Error opening file ../../roms/dmg-acid2.gb" << std::endl;
+        std::cout << "Error opening file ../work/roms/dmg-acid2.gb" << std::endl;
         return 1;
     }
 
